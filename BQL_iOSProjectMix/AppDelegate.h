a3b8ff3f2d764ad3be209b285ec3efee2613c209
biqinglin <931237936@qq.com>
2018-04-28 16:42:11 +0800

    Initial Commit

---
+++ BQL_iOSProjectMix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BQL_iOSProjectMix
+//
+//  Created by qinglin bi on 2018/4/28.
+//  Copyright © 2018年 qinglin bi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BQL_iOSProjectMix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BQL_iOSProjectMix
+//
+//  Created by qinglin bi on 2018/4/28.
+//  Copyright © 2018年 qinglin bi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
